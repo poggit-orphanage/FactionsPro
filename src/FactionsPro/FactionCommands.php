07ca58c03c7f98ed477197490e257f2b9afe1e81
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-01-18 17:48:24 +0000

    Update FactionCommands.php

--- src/FactionsPro/FactionCommands.php
+++ src/FactionsPro/FactionCommands.php
@@ -737,6 +737,7 @@ class FactionCommands {
                         }
                         $this->plugin->getPlayersInFactionByRank($sender, $args[1], "Leader");
                     }
+			    
                     ////////////////////////////// ALLY SYSTEM ////////////////////////////////
                     if (strtolower($args[0] == "enemy")) {
                         if (!isset($args[1])) {
