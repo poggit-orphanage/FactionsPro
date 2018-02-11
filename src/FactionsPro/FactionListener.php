5181a32c0ca7d25eacf5adc803f6a24e7b4bf5a0
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-02-11 16:33:07 +0000

    Revert.

--- src/FactionsPro/FactionListener.php
+++ src/FactionsPro/FactionListener.php
@@ -1,3 +1,4 @@
+
 <?php
 namespace FactionsPro;
 use pocketmine\plugin\PluginBase;
@@ -171,36 +172,6 @@ class FactionListener implements Listener {
         }
     }
     
-    public function onJoin(PlayerJoinEvent $ev) {
-          $p = $ev->getPlayer();
-		 $this->infac[] = $p->getName();
-	}
-   }
-
-   public function onMove(PlayerMoveEvent $ev){
-      $p = $ev->getPlayer();
-	  $name = $p->getName();
-	   $level = $sender->getPlayer()->getLevel();
-	   
-						if(!$this->FactionsPro->isInPlot($p)){
-							$id = array_search($p->getName(), $this->infac);
-							unset($this->infac[$id]);
-                        }else{
-						if (!in_array("$name", $this->infac)) {
-							 $x = floor($p->getX());
-							 $y = floor($p->getY());
-							 $z = floor($p->getZ());
-							 $fac = $this->FactionsPro->factionFromPoint($x, $z, $sender->getPlayer()->getLevel());
-							 $this->infac[] = $p->getName();
-							 $title = "§a§lNow Entering ";
-							 $subtitle = "§b§l" . $fac . " ";
-							 $p->addTitle($title, $subtitle);
-						}
-					}
-                            }
-	        }
-    }
-
     public function onBlockBreak(BlockBreakEvent $event){
 		if($event->isCancelled()) return;
 		$playerName = $event->getPlayer();
