a2ccd583a8d9677143683f9a61f5f5cb107e6d8c
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-06-24 21:00:37 +0100

    bug fix

--- src/FactionsPro/FactionCommands.php
+++ src/FactionsPro/FactionCommands.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace FactionsPro;
+
 use pocketmine\command\{Command, CommandSender};
 use pocketmine\{Server, Player};
 use pocketmine\utils\TextFormat;
 use pocketmine\math\Vector3;
 use pocketmine\level\{Level, Position};
+
 class FactionCommands {
 	
     public $plugin;
@@ -1660,19 +1663,20 @@ switch(strtolower($args[0])) {
 			$serverName = $this->plugin->prefs->get("ServerName");
 			if($args[1] == 7){
 				$sender->sendMessage(TextFormat::BLUE . "$serverName §dHelp §2[§57/7§2]" . TextFormat::RED . "\n§a/f donate|pay <amount> - §7Donate to a faction from your Eco Bank.\n§a/f withdraw|wd <amount> - §7With draw from your faction bank\n§a/f balance|bal - §7Checks your faction balance\n§a/f map|compass - §7Faction Map command\n§a/f overclaim|oc - §7Overclaims a plot.\n§a/f seebalance|sb - §7Checks other faction balances.");
-				if($sender->isOp()){
+				if($sender->isOp() == true){
 				$sender->sendMessage("§4§ldo /f help 8 to see OP Commands.");
 				return true;
 				break;
 				}
 			}
 			if($args[1] == 8){
-				$sender->isOP();
+				if($sender->isOp() == true){
 				$serverName = $this->plugin->prefs->get("ServerName");
 				$sender->sendMessage(TextFormat::BLUE . "$serverName §dHelp (OP Commands) §2[§51/1§2]" . TextFormat::RED . "\n§4/f addstrto <faction> <STR> - §cAdds Strength to a faction.\n§4/f addbalto <faction> <money> - §cAdds Money to a faction.\n§4/f forcedelete|fdisband <faction> - §cForce deletes a faction.\n§4/f forceunclaim|func <faction> - §cForce unclaims a plot / land.");
 				return true;
 			break;
-		        }
+				}
+			}
                      }
                 }
         } else {
